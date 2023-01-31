389ed9281a8f9fbf1849d734e49b5dada39782a7
nathanoliveira23 <nathanoliveira399@gmail.com>
2023-01-31 12:01:12 -0300

    feat: starting the habits form

---
+++ app/src/components/ProgressBar.tsx
@@ -0,0 +1,21 @@
+interface ProgressBarProps {
+  progress: number;
+}
+
+export function ProgressBar({ progress } : ProgressBarProps) {
+  const progressStyles = {
+    width: `${progress}%`,
+  }
+
+  return (
+    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+      <div
+        className="h-3 rounded-xl bg-violet-600 w-3/4"
+        role="progressbar"
+        aria-label="Progresso de hábitos completados nesse dia"
+        aria-valuenow={progress}
+        style={progressStyles}
+      />
+    </div>
+  );
+}
